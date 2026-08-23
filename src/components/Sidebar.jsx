734cc30b254e484b8a018f5e32d22ded86293cea
Paulo Ernesto Freiheit Côrtes <paulo-ernesto.cortes@hotmail.com>
2026-08-23 16:14:21 -0300

    Add Sidebar component with sign-out functionality and styles

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom";
+
+import CustomButton from "./CustomButton";
+
+import "./Sidebar.scss";
+import logo from "../assets/images/logo.png";
+
+const Sidebar = () => {
+    const navigate = useNavigate();
+
+    const handleSignOutClick = () => {
+        navigate("/login");
+    };
+    return (
+        <div className="sidebar-container">
+            <div className="logo">
+                <img src={logo} alt="Full Stack Club" />
+            </div>
+
+            <div className="sign-out">
+                <CustomButton onClick={handleSignOutClick}>Sair</CustomButton>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
