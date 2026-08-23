9e4e979014e142f8d0f868fd0eed9869c17e7cec
Paulo Ernesto Freiheit Côrtes <paulo-ernesto.cortes@hotmail.com>
2026-08-23 09:40:10 -0300

    Refactor app structure: replace App.js with App.jsx, add Tasks component, and implement task fetching and display

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,53 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "./Tasks.scss";
+import TaskItem from "./TaskItem";
+
+const Tasks = () => {
+    const [tasks, setTasks] = useState([]);
+
+    const fetchTasks = async () => {
+        try {
+            const { data } = await axios.get("http://localhost:8000/tasks");
+            setTasks(data);
+        } catch (error) {
+            console.error("Error fetching tasks:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchTasks();
+    }, []);
+
+    return (
+        <div className="tasks-container">
+            <h2>Minhas tarefas</h2>
+
+            <div className="last-tasks">
+                <h3>Últimas tarefas</h3>
+                <div className="tasks-list">
+                    {tasks
+                        .filter((task) => !task.isCompleted)
+                        .map((lastTask) => (
+                            <TaskItem task={lastTask} />
+                        ))}
+                </div>
+            </div>
+
+            <div>
+                <div className="completed-tasks">
+                    <h3>Tarefas concluídas</h3>
+                    <div className="tasks-list">
+                        {tasks
+                            .filter((task) => task.isCompleted)
+                            .map((completedTask) => (
+                                <TaskItem task={completedTask} />
+                            ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Tasks;
